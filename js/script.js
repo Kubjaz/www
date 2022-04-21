575d64fc671ad1a6f8c3a6166dd53521bd806ca8
OkPeach <lucienismeme@gmail.com>
2022-04-21 22:19:45 +0200

    99% dodělané cctv okno

--- js/script.js
+++ js/script.js
@@ -6,19 +6,21 @@ function displayOpen() {
 function displayClose() {
     document.getElementById("pupupdiv").style.visibility = "hidden";
     document.getElementById("pupupdiv").style.opacity = "0";
-    document.getElementById("cctvtext").remove();
+    
+    for (var i = 0; i < 11; i++) document.getElementById(`pupupdiv`).removeChild(document.getElementById(`cctvtext`));
 }
 
 function displayCCTV() {
     document.getElementById("cctv").style.display = "block";
 
-    $(`<br/>`).appendTo(document.getElementById(`pupupdiv`));
+    $(`<br id="cctvtext"/>`).appendTo(document.getElementById(`pupupdiv`));
     $(`<p class="cctvtext" id="cctvtext"/>`).text("Potřebujete do vašeho domu, firmy nebo jakéhokoli objektu nainstalovat kvalitní a cennově přístupný kamerový systém? Firma Security System Záleský s.r.o. je tu právě pro Vás! Nabízíme profesionální montáž kamerových systémů pro klienty po celé ČR. Samozřejmostí je vypracování nabídky, možnost servisní smlouvy a návrh kamerového systému na míru, přesně podle Vašich požadavků a potřeb.").appendTo(document.getElementById(`pupupdiv`));
+    $(`<img class="cctvimage" id="cctvtext"/>`).appendTo(document.getElementById(`pupupdiv`));
     $(`<p class="cctvtext" id="cctvtext"/>`).text("Naše firma instaluje domovní kamery a kamerové systémy v po celé České republice.").appendTo(document.getElementById(`pupupdiv`));
     $(`<p class="cctvtext" id="cctvtext"/>`).text("Vytváříme inteligentní kamerové systémy na míru, které Vám ušetří peníze. Kamerový systém po instalaci nadále sledujeme pro lepší nastavení s ohledem na denní a noční světlo.").appendTo(document.getElementById(`pupupdiv`));
     $(`<p class="cctvtext" id="cctvtext"/>`).text("K montáži kamerových systémů využíváme prvky od ověřených výrobců. Nejčastěji montujeme ověřeného dodavatele Hikvision.").appendTo(document.getElementById(`pupupdiv`));
 
-    $(`<br/>`).appendTo(document.getElementById(`pupupdiv`));
+    $(`<br id="cctvtext"/>`).appendTo(document.getElementById(`pupupdiv`));
 
     $(`<h2 class="cctvtextfat" id="cctvtext"/>`).text("JAK POSTUPOVAT V PŘÍPADĚ ZÁJMU O MONTÁŽ KAMEROVÉHO SYSTÉMU?").appendTo(document.getElementById(`pupupdiv`));
     $(`<p class="cctvtextodp" id="cctvtext"/>`).text("Kontaktujte nás a podrobně popište o jaký druh služby budete mít zájem.").appendTo(document.getElementById(`pupupdiv`));
